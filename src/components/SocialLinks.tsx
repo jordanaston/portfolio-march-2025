5c5117ae289be34be3b8b8b8b1545bc7574c32d4
Jordan Aston <jordanaston93@gmail.com>
2025-03-23 21:10:25 +1000

    removed mailto: and added a copy to clipboard for email since mailto: not working

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,6 +1,7 @@
 import { FaGithub, FaLinkedin, FaDiscord } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import { Button } from "./ui/button";
+import { toast } from "sonner";
 
 const socialLinks = [
   {
@@ -21,30 +22,44 @@ const socialLinks = [
   {
     icon: SiGmail,
     name: "Mail",
-    url: `mailto:${process.env.NEXT_PUBLIC_PERSONAL_EMAIL!}`,
+    email: "jordanaston93@gmail.com",
   },
 ] as const;
 
 export default function SocialLinks() {
+  const handleClick = (link: (typeof socialLinks)[number]) => {
+    if ("email" in link) {
+      navigator.clipboard.writeText(link.email);
+      toast.success("Email copied to clipboard!");
+      return;
+    }
+    window.open(link.url, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <div className="flex w-full flex-col items-center justify-between gap-[clamp(0.2rem,0.8vw+0.25rem,1rem)]">
       <p className="text-[clamp(0.35rem,0.7vw+0.3rem,0.875rem)]">
         FIND ME ONLINE
       </p>
       <div className="flex w-full flex-row items-center justify-between gap-[clamp(0.15rem,0.8vw+0.25rem,1rem)]">
-        {socialLinks.map(({ icon: Icon, name, url }) => (
-          <Button
-            key={name}
-            variant="outline"
-            className="h-[clamp(1rem,2vw+1rem,2.5rem)] rounded-none"
-            onClick={() => window.open(url, "_blank", "noopener,noreferrer")}
-          >
-            <div className="flex flex-row items-center justify-center gap-[clamp(0.1rem,0.4vw+0.15rem,0.5rem)] px-0 sm:px-[clamp(0.2rem,1.5vw+0.5rem,2rem)]">
-              <Icon className="size-[clamp(0.5rem,1.2vw+0.5rem,1.5rem)]" />
-              <p className="text-[clamp(0.4rem,0.8vw+0.3rem,1rem)]">{name}</p>
-            </div>
-          </Button>
-        ))}
+        {socialLinks.map((link) => {
+          const Icon = link.icon;
+          return (
+            <Button
+              key={link.name}
+              variant="outline"
+              className="h-[clamp(1rem,2vw+1rem,2.5rem)] rounded-none"
+              onClick={() => handleClick(link)}
+            >
+              <div className="flex flex-row items-center justify-center gap-[clamp(0.1rem,0.4vw+0.15rem,0.5rem)] px-0 sm:px-[clamp(0.2rem,1.5vw+0.5rem,2rem)]">
+                <Icon className="size-[clamp(0.5rem,1.2vw+0.5rem,1.5rem)]" />
+                <p className="text-[clamp(0.4rem,0.8vw+0.3rem,1rem)]">
+                  {link.name}
+                </p>
+              </div>
+            </Button>
+          );
+        })}
       </div>
     </div>
   );
