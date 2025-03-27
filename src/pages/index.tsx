b4aa5a47d3f8dc9b3dcc7b65b547e9b054cfd3ab
Jordan Aston <jordanaston93@gmail.com>
2025-03-27 15:50:19 +1000

    general positioning fixes for app content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,36 +18,36 @@ import BackToTop from "@/components/BackToTop";
 
 export default function Home() {
   return (
-    <div className="mb-48 min-h-screen">
+    <div className="mb-24 min-h-screen">
       <NarBar />
       <section id="home" className="relative z-30 w-screen">
-        <div className="mt-24 grid w-full grid-cols-[minmax(clamp(1rem,5vw,2rem),auto)_minmax(350px,1fr)_minmax(clamp(1rem,5vw,2rem),auto)] sm:mt-32 md:grid-cols-[minmax(20px,clamp(500px,30vw,750px))_minmax(350px,clamp(850px,50vw,1200px))_minmax(60px,1fr)]">
+        <div className="mt-24 grid w-full grid-cols-[minmax(clamp(1rem,5vw,2rem),auto)_minmax(350px,1fr)_minmax(clamp(1rem,5vw,2rem),auto)] sm:mt-32 md:grid-cols-[minmax(20px,clamp(500px,30vw,750px))_minmax(350px,clamp(850px,50vw,1200px))_minmax(60px,1fr)] 2xl:mt-38">
           <HeroSection />
           <div className="col-start-1 flex flex-col items-start justify-center" />
           <div className="col-start-2 mt-8 flex flex-col items-start justify-center sm:mt-16">
             <SocialLinks />
             <ScrollIndicator />
             <div className="h-[60vh]" />
-            <div className="mb-24 flex flex-col">
+            <div className="flex flex-col">
               <TextCard section={About()} fontWeight="medium" />
             </div>
           </div>
         </div>
       </section>
 
-      <section id="work" className="mx-auto max-w-[90vw] space-y-32 py-32">
+      <section id="work" className="mx-auto mt-24 max-w-[90vw]">
         <ProjectCard section={GlueNetwork()} />
         <ProjectCard section={LocalPegs()} />
         <ProjectCard section={ECommerceApp()} />
         <ProjectCard section={NbmForum()} />
       </section>
 
-      <section id="info" className="w-screen">
+      <section id="info" className="mt-36 w-screen">
         <div className="grid w-full grid-cols-[minmax(clamp(1rem,5vw,2rem),auto)_minmax(350px,1fr)_minmax(clamp(1rem,5vw,2rem),auto)] md:grid-cols-[minmax(20px,clamp(500px,30vw,750px))_minmax(350px,clamp(850px,50vw,1200px))_minmax(60px,1fr)]">
           <div className="col-start-2 flex flex-col">
-            <div className="mb-48 flex flex-col">
+            <div className="flex flex-col">
               <TextCard section={Info()} size="small" />
-              <div className="mt-48">
+              <div className="mt-40">
                 <p className="mb-4 text-[clamp(0.75rem,1vw+0.5rem,1rem)]">
                   MORE PROJECTS
                 </p>
@@ -58,7 +58,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="contact" className="mx-auto max-w-[90vw]">
+      <section id="contact" className="mx-auto mt-40 max-w-[90vw]">
         <div className="mx-auto max-w-2xl">
           <h2 className="mb-8 text-[clamp(1.25rem,2vw,1.75rem)] font-medium">
             GET IN TOUCH
@@ -66,7 +66,7 @@ export default function Home() {
           <ContactForm />
         </div>
       </section>
-      <div className="mx-auto mt-48 max-w-[70vw]">
+      <div className="mx-auto mt-56 max-w-[70vw]">
         <TechStack />
       </div>
       <BackToTop />
